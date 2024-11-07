ac2135553e96b9e8ba8ba8ba1fc0168b4f87d85f
selvakumarj1807 <147826154+selvakumarj1807@users.noreply.github.com>
2024-11-07 17:11:16 +0530

    Update Index.sql

--- sql_notes/Index.sql
+++ sql_notes/Index.sql
@@ -1,102 +1,237 @@
-INDEX 
+/*
+Index :
+-------
+      1. Fast retrieval by effectively managing data
+      2. It is used retrive the data faster depands on optimizer's decision(Count of retrieval data)
+      
+Type's of index :
+-----------------
+      1. b-tree
+      2. bitmap
+      3. unique
+      4. function based
+      5. composite 
+      6. index organized table 
 
-    It is used to improve the performance of the query or used to reduce the cost of the query
-    
-TYPES OF INDEX 
-
-    1.B-Tree Index 
-    2.Bitmap Index 
-    3.Composite Index
-    4.Unique Index 
-    5.Function based Index 
-    6.Index organized table
-    
---__________________________________________________________
-1.B-Tree Index
-
-    -- Balanced tree index, (ie) it is an ordered list of values that are divided into Range associated with rowid 
-    -- It is created on high cardinality column/s 
-	-- high cardinality column/s  = Unique RECORS in a column/s
+--1. b-tree index :
+-------------------
+    1. Balanced tree index 
+    2. It is created for high cardinality column/s 
+    3. high cardinality column/s  = Unique RECORS in a column/s
+    4. declare Less Duplicate value column 
     
 Syntax:
     Create index index_name on table_name(column_name);
 
-select * from employees;
-   
-create table employ as select * from employees;
-   
-select * from employ;
+*/
+create table employee_copy as select * from employees;
 
-select * from employ where first_name='David';
-
-create index idx_1 on employ(first_name);
+select * from employee_copy;
 
 select * from user_indexes;
 
-select * from user_indexes where table_name='EMPLOY';
+select * from user_indexes where table_name = 'EMPLOYEE_COPY';
 
-select * from user_ind_columns where table_name='EMPLOY';
+select * from user_ind_columns where table_name = 'EMPLOYEE_COPY';
 
+-- Check before index creation
 
---_________________________________________
+select * from employee_copy where first_name like 'A%';
 
+explain plan for select * from employee_copy where first_name like 'A%';
 
-2.Bitmap index
+select dbms_xplan.display from dual;
 
-    -- Stores the data in a form of bitmap array .ie. stores the index key column in formation along with its rowid
-    -- It is created on low cardinality columns
-    -- low cardinality columns = large number of duplicate records in a columns
-    
+-- Check after index creation
+
+create index fname_idx1 on employee_copy(first_name);
+
+select * from user_indexes where table_name = 'EMPLOYEE_COPY';
+
+select * from user_ind_columns where table_name = 'EMPLOYEE_COPY';
+
+select * from employee_copy where first_name like 'A%';
+
+explain plan for select * from employee_copy where first_name like 'A%';
+
+select dbms_xplan.display from dual;
+
+--------------------------------------------------------------------------------
+
+/*
+--2. bitmap index :
+-------------------
+      1. It is created for low cardinality columns
+      2. low cardinality columns = large number of duplicate records in a columns
+      3. declare More Duplicate value column
+
 Syntax:
     Create bitmap index index_name on table_name(column_name);
-    
---______________________________
+            
+*/
+-- Check before index creation
 
-3.Composite Index
+select * from employee_copy where department_id = 90;
 
-    -- Created on multiple columns in a table
-    -- It is also called as concatenated index
-    
-Syntax:
-    Create index index_name on table_name(column_name(1),,,...,,column_name(n));
-    
---______________________________
+explain plan for select * from employee_copy where department_id = 90;
 
-4.Unique Index
+select dbms_xplan.display from dual;
 
-    -- Does not allow duplicate values and allow null values
+-- Check after index creation
 
-    
-Syntax:
-    Create unique index index_name on table_name(column_name(1),,,...,,column_name(n));
-    
---______________________________
+create bitmap index depid_idx1 on employee_copy(department_id);
 
-5.Function based Index
+select * from employee_copy where department_id = 90;
 
-    -- If an index is created using the function or expression or expression then it is function based index
-    -- It can be either B-tree index or a Bit-map index
+explain plan for select * from employee_copy where department_id = 90;
 
+select dbms_xplan.display from dual;
+
+select * from employee_copy where department_id = 50;
+
+explain plan for select * from employee_copy where department_id = 50;
+
+select dbms_xplan.display from dual;
+
+--------------------------------------------------------------------------------
+
+/*
+--3. unique index :
+          declare unique value column
+*/
+-- Check before index creation
+
+select * from employee_copy where employee_id = 100;
+
+explain plan for select * from employee_copy where employee_id = 100;
+
+select dbms_xplan.display from dual;
+
+-- Check after index creation
+
+create unique index empid_idx1 on employee_copy(employee_id);
+
+select * from employee_copy where employee_id = 100;
+
+explain plan for select * from employee_copy where employee_id = 100;
+
+select dbms_xplan.display from dual;
+
+--------------------------------------------------------------------------------
+
+/*
+--4 Function based Index :
+--------------------------
+
+    If an index is created using the function or expression then it is function based index
     
 Syntax:
     Create index index_name on table_name(Function | Expreeion);
+ 
+*/
+
+-- Check before index creation
+
+select * from employee_copy where upper(first_name) = 'LEX';
+
+explain plan for select * from employee_copy where upper(first_name) = 'LEX';
+
+select dbms_xplan.display from dual;
+
+-- Check after index creation
+
+create index idx_fname on employee_copy(upper(first_name));
+
+select * from employee_copy where upper(first_name) = 'LEX';
+
+explain plan for select * from employee_copy where upper(first_name) = 'LEX';
+
+select dbms_xplan.display from dual;
+
+--------------------------------------------------------------------------------
+
+/*
+--5. Composite Index :
+----------------------
+      1. Index created for multiple columns 
+      2. It is also called as concatenated index
+      3. Accepted 32 columns
+      
+Syntax:
+    Create index index_name on table_name(column_name(1),,,...,,column_name(n));
     
---______________________________
+*/
 
-6.Index Organized Table
+-- Check before index creation
 
-    -- Organizing the data at the time of inserting
-    -- You must have primary key on the table
-    -- Cannot able to partition an index organized table
+select * from employee_copy where department_id = 50 and manager_id = 120;
 
-    
+explain plan for select * from employee_copy where department_id = 50 and manager_id = 120;
+
+select dbms_xplan.display from dual;
+
+-- Check after index creation
+
+create index idx_did_mid on employee_copy(department_id, manager_id);
+
+select * from employee_copy where department_id = 50 and manager_id = 120;
+
+explain plan for select * from employee_copy where department_id = 50 and manager_id = 120;
+
+select dbms_xplan.display from dual;
+
+--------------------------------------------------------------------------------
+
+/*
+--6 IndexOrganized table :
+--------------------------
+      1. Organizing the data at the time of inserting
+      2. You must have primary key on the table
+      3. Cannot able to partition an index organized table
+
 Syntax:
     Create table table_name(column_name datatype,,,..,,,columnname datatype) organization index ;
     
---______________________________
+*/
 
+-- Check before IndexOrganized table creation
 
+create table studentDetails(stud_id number, stud_name varchar2(50));
 
+select * from studentDetails;
 
+insert into studentDetails values(2, 'name2');
 
+insert into studentDetails values(4, 'name4');
 
+insert into studentDetails values(3, 'name3');
+
+insert into studentDetails values(5, 'name5');
+
+insert into studentDetails values(1, 'name1');
+
+select * from studentDetails;
+
+-- Check after IndexOrganized table creation
+
+create table studentDetails02(stud_id number primary key, stud_name varchar2(50)) organization index;
+
+select * from studentDetails02;
+
+insert into studentDetails02 values(2, 'name2');
+
+insert into studentDetails02 values(4, 'name4');
+
+insert into studentDetails02 values(3, 'name3');
+
+insert into studentDetails02 values(5, 'name5');
+
+insert into studentDetails02 values(1, 'name1');
+
+select * from studentDetails02;
+
+--------------------------------------------------------------------------------
+
+drop table studentDetails;
+
+drop index depid_idx1;
